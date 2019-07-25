9aa502a6bce651c7f7c262ab552174bbd46e61a4
Shahar Kazaz <shahar.kazaz@gmail.com>
2019-07-25 10:17:09 +0300

    specs(Headless) - set basic app spec

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,35 +1,21 @@
-// import { TestBed, async } from '@angular/core/testing';
-// import { RouterTestingModule } from '@angular/router/testing';
-// import { AppComponent } from './app.component';
-//
-// describe('AppComponent', () => {
-//   beforeEach(async(() => {
-//     TestBed.configureTestingModule({
-//       imports: [
-//         RouterTestingModule
-//       ],
-//       declarations: [
-//         AppComponent
-//       ],
-//     }).compileComponents();
-//   }));
-//
-//   it('should create the app', () => {
-//     const fixture = TestBed.createComponent(AppComponent);
-//     const app = fixture.debugElement.componentInstance;
-//     expect(app).toBeTruthy();
-//   });
-//
-//   it(`should have as title 'ngx-translate'`, () => {
-//     const fixture = TestBed.createComponent(AppComponent);
-//     const app = fixture.debugElement.componentInstance;
-//     expect(app.title).toEqual('ngx-translate');
-//   });
-//
-//   it('should render title in a h1 tag', () => {
-//     const fixture = TestBed.createComponent(AppComponent);
-//     fixture.detectChanges();
-//     const compiled = fixture.debugElement.nativeElement;
-//     expect(compiled.querySelector('h1').textContent).toContain('Welcome to ngx-translate!');
-//   });
-// });
+import { AppComponent } from './app.component';
+import { createTestComponentFactory, Spectator } from '@netbasal/spectator';
+import { providersMock } from '../../projects/transloco/src/lib/tests/transloco.mocks';
+import { RouterModule } from '@angular/router';
+import { MockModule } from 'ng-mocks';
+
+describe('AppComponent', () => {
+  let host: Spectator<AppComponent>;
+  const createHost = createTestComponentFactory({
+    component: AppComponent,
+    imports: [MockModule(RouterModule)],
+    providers: providersMock
+  });
+  beforeEach(() => {
+    host = createHost();
+  });
+
+  it('should be defined', () => {
+    expect(host.component).toExist();
+  });
+});
