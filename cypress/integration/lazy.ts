f06c922e4d50704847e5b4970b5ebf9279c3e03e
itay <itayoded1@gmail.com>
2019-07-30 21:52:40 +0300

    test: added e2e test to lazy page
    re #33

---
+++ cypress/integration/lazy.ts
@@ -0,0 +1,40 @@
+export function generateContentLoader() {
+  cy.get(`[data-cy=lazy-page-loading]`).should('contain', `Loading...`);
+}
+
+export function generateContent(lang = 'english') {
+  // Structural Directive
+  cy.get(`[data-cy=regular]`).should('contain', `Admin ${lang}`);
+  cy.get(`[data-cy=lazy-page]`).should('contain', `Admin Lazy ${lang}`);
+}
+
+export function generateContentWithoutLoader() {
+  // Structural Directive
+  cy.get(`[data-cy=lazy-page-loading]`).should('not.exist');
+}
+
+describe('Transloco', () => {
+  beforeEach(() => {
+    cy.visit('/lazy');
+  });
+
+  it('should display loading template', () => {
+    generateContentLoader();
+  });
+
+  it('should display lazy translation', () => {
+    cy.get(`[data-cy=es]`).click();
+
+    generateContent('spanish');
+
+    cy.get(`[data-cy=en]`).click();
+    generateContent();
+  });
+
+  it('should not display loader template after loaded once', () => {
+    cy.get(`[data-cy=es]`).click();
+    cy.get(`[data-cy=en]`).click();
+
+    generateContentWithoutLoader();
+  });
+});
