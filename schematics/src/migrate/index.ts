55e00b71cf86f1358541c0f39618bcc847f385f3
Itay Oded <itayoded1@gmail.com>
2019-08-12 17:48:03 +0300

    chore(schematics): add command for migration (#3)

---
+++ schematics/src/migrate/index.ts
@@ -0,0 +1,18 @@
+import { Rule, SchematicsException, noop } from '@angular-devkit/schematics';
+import { run } from './migration';
+import { SchemaOptions } from './schema';
+
+export default function(options: SchemaOptions): Rule {
+  return () => {
+    if (!options.path) {
+      throw new SchematicsException(`
+        Please specify the root source of your project.
+        (e.g. --path ./src)
+      `);
+    }
+
+    run(options.path);
+
+    return noop();
+  };
+}
