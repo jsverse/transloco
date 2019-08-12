2a9d0a4d12149392ddedbbeaf6c300f101f993fc
Shahar Kazaz <shahar.kazaz@gmail.com>
2019-08-12 22:41:01 +0300

    fix: 🐛 get value function
    fix get value function in cases where the obj key is as 'a.b.c'

--- projects/ngneat/transloco/src/lib/helpers.ts
+++ projects/ngneat/transloco/src/lib/helpers.ts
@@ -1,4 +1,8 @@
 export function getValue(obj: object, path: string) {
+  /* For cases where the key is like: 'general.something.thing' */
+  if (obj.hasOwnProperty(path)) {
+    return obj[path];
+  }
   return path.split('.').reduce((p, c) => (p && p[c]) || null, obj);
 }
 
