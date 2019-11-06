7f72d3939f0cb7fd5913724db8b54c49e9ccd356
itay <itayoded1@gmail.com>
2019-11-06 11:11:31 +0200

    feat: 🎸 add schematics migration script from angular

--- schematics/src/migrate/index.ts
+++ schematics/src/migrate/index.ts
@@ -1,9 +1,11 @@
-import { Rule, SchematicsException, noop } from '@angular-devkit/schematics';
+import { Rule, SchematicsException, schematic } from '@angular-devkit/schematics';
+import { fromPromise } from 'rxjs/internal-compatibility';
+import { map } from 'rxjs/operators';
 import { run } from './ngx-translate-migration';
 import { SchemaOptions } from './schema';
 
 export default function(options: SchemaOptions): Rule {
-  return () => {
+  return tree => {
     if (!options.path) {
       throw new SchematicsException(`
         Please specify the root source of your project.
@@ -11,6 +13,8 @@ export default function(options: SchemaOptions): Rule {
       `);
     }
 
-    return run(options.path);
+    return options.from === 'Angular'
+      ? schematic('ng-migrate', options)
+      : fromPromise(run(options.path)).pipe(map(() => tree));
   };
 }
---
+++ schematics/src/ng-migrate/schema.ts
@@ -0,0 +1,14 @@
+export interface SchemaOptions {
+  /**
+   * The path to the source root directory.
+   */
+  path: string;
+  /**
+   * The location of the translation files.
+   */
+  translationFilesPath: string;
+  /**
+   * The languages of the project.
+   */
+  langs: string;
+}
