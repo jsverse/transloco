5498deeb21d659ada26763230316e0d284afb772
Netanel Basal <NetanelBasal@users.noreply.github.com>
2020-01-01 07:41:31 +0200

    fix: 🐛 same fallback lang should not load again (#211)
    Closes: #210

--- src/app/inline-loaders/inline-loaders.module.ts
+++ src/app/inline-loaders/inline-loaders.module.ts
@@ -4,7 +4,7 @@ import { InlineLoadersRoutingModule } from './inline-loaders-routing.module';
 import { InlineComponent } from './inline/inline.component';
 import { TRANSLOCO_SCOPE, TranslocoModule } from '@ngneat/transloco';
 
-const loader = ['en'].reduce((acc: any, lang: string) => {
+const loader = ['en', 'es'].reduce((acc: any, lang: string) => {
   acc[lang] = () => import(`./i18n/${lang}.json`);
   return acc;
 }, {});
