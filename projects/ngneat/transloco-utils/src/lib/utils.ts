31c180a23463e11d2941ddf7c5bf2bc19dea929b
Shahar Kazaz <shahar.kazaz@gmail.com>
2020-01-12 23:43:04 +0200

    feat: 🎸 add support for getting config from path

--- projects/ngneat/transloco-utils/src/lib/utils.ts
+++ projects/ngneat/transloco-utils/src/lib/utils.ts
@@ -1,9 +1,12 @@
 import { TranslocoConfig } from './types';
-const cosmiconfig = require('cosmiconfig');
+const { cosmiconfigSync } = require('cosmiconfig');
 
-export function getConfig(): TranslocoConfig {
-  const explorer = cosmiconfig('transloco');
-  const configSearch = explorer.searchSync();
+export function getConfig(path?: string): TranslocoConfig {
+  const explorer = cosmiconfigSync('transloco');
+  if (path) {
+    path = `${process.cwd()}/${path}`;
+  }
+  const configSearch = explorer.search(path);
 
   return configSearch ? configSearch.config : {};
 }
