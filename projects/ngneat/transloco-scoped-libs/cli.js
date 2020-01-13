c8f13a00d3772d0e3c954f646726d741f5b70b35
Dorian Sobacki <dorian.sobacki@banqsoft.pl>
2020-01-13 11:03:47 +0100

    plugin(transloco-scoped-libs): adds modifyGitignore option
    Adds an option to the transloco-scoped-libs that allows not to modify the .gitignore file at all
    
    resolves #216

--- projects/ngneat/transloco-scoped-libs/cli.js
+++ projects/ngneat/transloco-scoped-libs/cli.js
@@ -4,12 +4,16 @@ const translocoUtils = require('@ngneat/transloco-utils');
 const { run } = require('./index');
 const commandLineArgs = require('command-line-args');
 
-const optionDefinitions = [{ name: 'watch', alias: 'w', type: Boolean, defaultValue: false }];
+const optionDefinitions = [
+  { name: 'watch', alias: 'w', type: Boolean, defaultValue: false },
+  { name: 'modifyGitignore', alias: 'm', type: Boolean, defaultValue: true }
+];
 const cliParams = commandLineArgs(optionDefinitions);
 const config = translocoUtils.getConfig();
 
 run({
   watch: cliParams.watch,
+  modifyGitignore: cliParams.modifyGitignore,
   rootTranslationsPath: config.rootTranslationsPath,
   scopedLibs: config.scopedLibs
 });
