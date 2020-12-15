1666f5d30b0b72270c2de52465ebbca33a7d1e91
shaharkazaz <shahar.kazaz@gmail.com>
2020-12-15 23:28:54 +0200

    fix: 🐛 service not initialized in testing module
    Deprecated the withLangs function in the testing module and created
    withOptions
    
    Closes: #250

--- projects/ngneat/transloco/src/lib/transloco-testing.module.ts
+++ projects/ngneat/transloco/src/lib/transloco-testing.module.ts
@@ -1,33 +1,105 @@
-import { Inject, Injectable, ModuleWithProviders, NgModule } from '@angular/core';
+import { APP_INITIALIZER, Inject, Injectable, InjectionToken, ModuleWithProviders, NgModule } from '@angular/core';
 import { TRANSLOCO_LOADER, TranslocoLoader } from './transloco.loader';
 import { HashMap, Translation } from './types';
 import { Observable, of } from 'rxjs';
 import { defaultProviders, TranslocoModule } from './transloco.module';
-import { TranslocoConfig, TRANSLOCO_CONFIG, translocoConfig } from './transloco.config';
+import { TRANSLOCO_CONFIG, TranslocoConfig, translocoConfig } from './transloco.config';
+import { TranslocoService } from './transloco.service';
+
+export interface TranslocoTestingOptions {
+  translocoConfig?: Partial<TranslocoConfig>;
+  preloadLangs?: boolean;
+  langs: HashMap<Translation>;
+}
+
+const TRANSLOCO_TEST_LANGS = new InjectionToken<HashMap<Translation>>(
+  'TRANSLOCO_TEST_LANGS - Available testing languages'
+);
+const TRANSLOCO_TEST_OPTIONS = new InjectionToken<TranslocoTestingOptions>('TRANSLOCO_TEST_OPTIONS - Testing options');
 
 @Injectable()
 export class TestingLoader implements TranslocoLoader {
-  constructor(@Inject('translocoLangs') private langs: HashMap<Translation>) {}
+  constructor(@Inject(TRANSLOCO_TEST_LANGS) private langs: HashMap<Translation>) {}
 
   getTranslation(lang: string): Observable<Translation> | Promise<Translation> {
     return of(this.langs[lang]);
   }
 }
 
+export function initTranslocoService(
+  service: TranslocoService,
+  langs: HashMap<Translation> = {},
+  options: TranslocoTestingOptions
+) {
+  const preloadAllLangs = () =>
+    options.preloadLangs
+      ? Promise.all(Object.keys(langs).map(lang => service.load(lang).toPromise()))
+      : Promise.resolve();
+
+  return preloadAllLangs;
+}
+
 @NgModule({
   exports: [TranslocoModule]
 })
 export class TranslocoTestingModule {
+  static withOptions(options: TranslocoTestingOptions) {
+    return {
+      ngModule: TranslocoTestingModule,
+      providers: [
+        {
+          provide: TRANSLOCO_TEST_LANGS,
+          useValue: options.langs
+        },
+        {
+          provide: TRANSLOCO_TEST_OPTIONS,
+          useValue: options
+        },
+        {
+          provide: APP_INITIALIZER,
+          useFactory: initTranslocoService,
+          deps: [TranslocoService, TRANSLOCO_TEST_LANGS, TRANSLOCO_TEST_OPTIONS],
+          multi: true
+        },
+        {
+          provide: TRANSLOCO_LOADER,
+          useClass: TestingLoader
+        },
+        defaultProviders,
+        {
+          provide: TRANSLOCO_CONFIG,
+          useValue: translocoConfig({
+            prodMode: true,
+            missingHandler: { logMissingKey: false },
+            ...options.translocoConfig
+          })
+        }
+      ]
+    };
+  }
+
+  /** @deprecated - use withOptions instead */
   static withLangs(
     langs: HashMap<Translation>,
-    config: Partial<TranslocoConfig> = {}
+    config: Partial<TranslocoConfig> = {},
+    options: Partial<TranslocoTestingOptions> = {}
   ): ModuleWithProviders<TranslocoTestingModule> {
     return {
       ngModule: TranslocoTestingModule,
       providers: [
         {
-          provide: 'translocoLangs',
+          provide: TRANSLOCO_TEST_LANGS,
           useValue: langs
+        },
+        {
+          provide: TRANSLOCO_TEST_OPTIONS,
+          useValue: options
+        },
+        {
+          provide: APP_INITIALIZER,
+          useFactory: initTranslocoService,
+          deps: [TranslocoService, TRANSLOCO_TEST_LANGS, TRANSLOCO_TEST_OPTIONS],
+          multi: true
         },
         {
           provide: TRANSLOCO_LOADER,
--- src/app/transloco-testing.module.ts
+++ src/app/transloco-testing.module.ts
@@ -1,4 +1,4 @@
-import { TranslocoConfig, TranslocoTestingModule } from '@ngneat/transloco';
+import { TranslocoConfig, TranslocoTestingModule, TranslocoTestingOptions } from '@ngneat/transloco';
 import en from '../assets/i18n/en.json';
 import es from '../assets/i18n/es.json';
 import admin from '../assets/i18n/admin-page/en.json';
@@ -6,20 +6,24 @@ import adminSpanish from '../assets/i18n/admin-page/es.json';
 import lazy from '../assets/i18n/lazy-page/en.json';
 import lazySpanish from '../assets/i18n/lazy-page/es.json';
 
-export function getTranslocoModule(config: Partial<TranslocoConfig> = {}) {
-  return TranslocoTestingModule.withLangs(
-    {
+export function getTranslocoModule(
+  config: Partial<TranslocoConfig> = {},
+  options: Partial<TranslocoTestingOptions> = {}
+) {
+  return TranslocoTestingModule.withOptions({
+    langs: {
       en,
       es,
       'admin-page/en': admin,
       'admin-page/es': adminSpanish,
-      'lazy-page/en' : lazy,
-      'lazy-page/es' : lazySpanish
+      'lazy-page/en': lazy,
+      'lazy-page/es': lazySpanish
     },
-    {
+    translocoConfig: {
       availableLangs: ['es', 'en'],
       defaultLang: 'es',
       ...config
-    }
-  );
+    },
+    ...options
+  });
 }
