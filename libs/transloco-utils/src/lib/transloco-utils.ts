28c7e9406b03cd222b20d9f1cfe202ce09f52656
shaharkazaz <shahar.kazaz@gmail.com>
2021-10-28 21:52:40 +0300

    refactor(utils): 💡 fix lint

--- libs/transloco-utils/src/lib/transloco-utils.ts
+++ libs/transloco-utils/src/lib/transloco-utils.ts
@@ -2,9 +2,7 @@ import { cosmiconfigSync } from 'cosmiconfig';
 import * as path from 'path';
 import { TranslocoGlobalConfig } from './transloco-utils.types';
 
-export function getGlobalConfig(
-  searchPath: string = ''
-): TranslocoGlobalConfig {
+export function getGlobalConfig(searchPath = ''): TranslocoGlobalConfig {
   const explorer = cosmiconfigSync('transloco');
   const resolvedPath = path.resolve(process.cwd(), searchPath);
   const configSearch = explorer.search(resolvedPath);
