29afbf9164607c7e941451031a8f6bf426ff3a6d
shaharkazaz <shahar.kazaz@gmail.com>
2021-10-28 21:04:03 +0300

    fix(utils): 🐛 wrong config search

--- libs/transloco-utils/src/lib/transloco-utils.ts
+++ libs/transloco-utils/src/lib/transloco-utils.ts
@@ -2,11 +2,9 @@ import { cosmiconfigSync } from 'cosmiconfig';
 import * as path from 'path';
 import { TranslocoGlobalConfig } from './transloco-utils.types';
 
-export function getGlobalConfig(searchPath?: string): TranslocoGlobalConfig {
-  if (!searchPath) {
-    return {};
-  }
-
+export function getGlobalConfig(
+  searchPath: string = ''
+): TranslocoGlobalConfig {
   const explorer = cosmiconfigSync('transloco');
   const resolvedPath = path.resolve(process.cwd(), searchPath);
   const configSearch = explorer.search(resolvedPath);
