2cf1460abfe18b8103a55e489cee870834e6d467
shaharkazaz <shahar.kazaz@gmail.com>
2022-12-11 13:20:26 +0200

    test(transloco): 💍 fix e2e

--- apps/transloco-playground/src/main.ts
+++ apps/transloco-playground/src/main.ts
@@ -1,10 +1,9 @@
-import { enableProdMode } from '@angular/core';
+import {enableProdMode, isDevMode} from '@angular/core';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 import { AppModule } from './app/app.module';
-import { environment } from './environments/environment';
 
-if (environment.production) {
+if (!isDevMode()) {
   enableProdMode();
 }
 
