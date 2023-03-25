032601ccb7cb573dae74a7808d517c5ac92724df
ben.12 <benmor_12@yahoo.fr>
2023-03-25 12:32:16 +0100

    fix(#621): transloco-validation - Fix arguments reading

--- libs/transloco-validator/src/index.ts
+++ libs/transloco-validator/src/index.ts
@@ -5,8 +5,8 @@ import validator from './lib/transloco-validator';
 
 const optionDefinitions: commandLineArgs.OptionDefinition[] = [
     { name: 'interpolationForbiddenChars', type: String, defaultValue: '{}' },
-    { name: 'file', alias: 'f', type: String, multiple: true, defaultOption: true },
+    { name: 'file', alias: 'f', type: String, multiple: true, defaultValue: [], defaultOption: true },
   ];
 
-const { interpolationForbiddenChars, translationFilePaths } = commandLineArgs(optionDefinitions);
-validator(interpolationForbiddenChars, translationFilePaths);
+const { interpolationForbiddenChars, file } = commandLineArgs(optionDefinitions);
+validator(interpolationForbiddenChars, file);
