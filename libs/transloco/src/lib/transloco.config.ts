53c8b1e6d62847118062d688cdd52dfe70ef5a00
Baptiste Moreau <baptiste.moreau@fime.com>
2025-09-14 14:24:16 +0200

    feat(transloco): 🎸 Add config option for scope auto prefixing
    Add an option to the config, to control if the TranslateService should
    prefix (or not) the translation key with the scope when provided. It fix
    an issue when a scope has been provided, the root translations keys
    can't be used with the TranslocoService methods due to auto-prefixing of
    the scope to them.
    
    ✅ Closes: #855

--- libs/transloco/src/lib/transloco.config.ts
+++ libs/transloco/src/lib/transloco.config.ts
@@ -20,6 +20,7 @@ export interface TranslocoConfig {
   interpolation: [string, string];
   scopes: {
     keepCasing?: boolean;
+    autoPrefixKeys?: boolean;
   };
 }
 
@@ -49,6 +50,7 @@ export const defaultConfig: TranslocoConfig = {
   interpolation: ['{{', '}}'],
   scopes: {
     keepCasing: false,
+    autoPrefixKeys: true,
   },
 };
 
