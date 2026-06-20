9d4718ced74cdf44af36923cf627cca69b5ea663
Shahar Kazaz <shahar.kazaz@gmail.com>
2026-06-20 15:40:39 +0300

    refactor: move provideZoneChangeDetection into appConfig

--- apps/transloco-playground/src/main.ts
+++ apps/transloco-playground/src/main.ts
@@ -1,10 +1,8 @@
-import { provideZoneChangeDetection } from '@angular/core';
 import { bootstrapApplication } from '@angular/platform-browser';
 
 import { AppComponent } from './app/app.component';
 import { appConfig } from './app/app.conifg';
 
-bootstrapApplication(AppComponent, {
-  ...appConfig,
-  providers: [provideZoneChangeDetection(), ...appConfig.providers],
-}).catch((err) => console.error(err));
+bootstrapApplication(AppComponent, appConfig).catch((err) =>
+  console.error(err),
+);
