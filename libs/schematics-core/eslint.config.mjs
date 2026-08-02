2adcc96c7734776ff46af1a4d9cd28ee83921905
Mohamed Ben Makhlouf <med.b.makhlouf@gmail.com>
2026-08-02 02:41:17 +0100

    feat: update jsonc-eslint-parser to version 3.1.0 and adjust import syntax

--- libs/schematics-core/eslint.config.mjs
+++ libs/schematics-core/eslint.config.mjs
@@ -1,5 +1,5 @@
 import baseConfig from '../../eslint.config.mjs';
-import jsoncEslintParser from 'jsonc-eslint-parser';
+import * as jsoncEslintParser from 'jsonc-eslint-parser';
 
 export default [
   ...baseConfig,
