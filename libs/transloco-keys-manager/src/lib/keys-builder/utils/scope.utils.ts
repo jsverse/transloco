daff76eea65bad4d35ded0a48ddcd6c46affe450
Mohamed Ben Makhlouf <med.b.makhlouf@gmail.com>
2026-08-22 22:56:25 +0200

    feat: improve scope management by enhancing alias handling and adding new tests

--- libs/transloco-keys-manager/src/lib/keys-builder/utils/scope.utils.ts
+++ libs/transloco-keys-manager/src/lib/keys-builder/utils/scope.utils.ts
@@ -4,18 +4,6 @@ let scopeToAlias: Scopes['scopeToAlias'] = {};
 let aliasToScope: Scopes['aliasToScope'] = {};
 
 export function addScope(scope: string, alias: string) {
-  // Keep the maps bijective, an overwritten scope or alias must not leave a
-  // stale reverse entry behind.
-  const previousAlias = scopeToAlias[scope];
-  if (previousAlias !== undefined) {
-    delete aliasToScope[previousAlias];
-  }
-
-  const previousScope = aliasToScope[alias];
-  if (previousScope !== undefined) {
-    delete scopeToAlias[previousScope];
-  }
-
   scopeToAlias[scope] = alias;
   aliasToScope[alias] = scope;
 }
