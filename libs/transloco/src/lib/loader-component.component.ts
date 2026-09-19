db9970d6771063762803b4490b5c9938ed9f3851
Mohamed Ben Makhlouf <med.b.makhlouf@gmail.com>
2026-09-19 12:54:02 +0200

    refactor(transloco): use OnPush for loader component

--- libs/transloco/src/lib/loader-component.component.ts
+++ libs/transloco/src/lib/loader-component.component.ts
@@ -1,9 +1,10 @@
-import { Component, input } from '@angular/core';
+import { ChangeDetectionStrategy, Component, input } from '@angular/core';
 
 @Component({
   template: `
     <div class="transloco-loader-template" [innerHTML]="html()"></div>
   `,
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class TranslocoLoaderComponent {
   readonly html = input<string>();
